dfadfd8ae621b823d7f99066ec76f30b654c3cc2
Akash Biswas <akash41bt@gmail.com>
2025-08-09 19:40:14 +0600

    detect cycle in a linked list using slow & fast pointer

---
+++ Linked List/Singly Linked List/cycle-linked-list.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode* next;
+
+    ListNode():val(0), next(nullptr){}
+
+    ListNode(int val):val(val), next(nullptr){}
+};
+
+bool hasCycle(ListNode* head){
+    ListNode* slow = head, *fast = head;
+
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow == fast)
+            return true;
+    }
+
+    return false;
+}
+
+int main(){
+    ListNode* head = new ListNode(3);
+
+    head->next = new ListNode(5);
+
+    head->next->next = new ListNode(7);
+
+    head->next->next->next = new ListNode(2);
+
+   // head->next->next->next->next = head->next;
+
+    if(hasCycle(head))
+        cout << "Cycle found" << endl;
+    else
+        cout << "Cycle not found" << endl;
+}
