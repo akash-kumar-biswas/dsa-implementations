5a09699ae15ccb93cbdd1fcb7ea6c111b01027a1
Akash Biswas <akash41bt@gmail.com>
2025-09-01 20:27:22 +0600

    find gcd and lcm of two numbers using euclidean algorithm

--- Number Theory/gcd-and-lcm-of-two-numbers.cpp
+++ Number Theory/gcd-and-lcm-of-two-numbers.cpp
@@ -7,11 +7,18 @@ int gcd(int a, int b){
     return gcd(b, a % b);       // gcd(a, b) = gcd (b, a % b) , for a > b or a < b.Works for both
 }
 
+int lcm(int a, int b){
+    return (a / gcd(a, b)) * b;   // LCM(a, b) * GCD(a, b) = a * b;
+}
+
 int main(){
     int a, b;
     cout << "Enter two numbers a and b: ";   // one number should be non-zero
     cin>> a >> b;  
 
     cout << "GCD of "<< a << " and " << b << " is: " << gcd(a, b);
+    cout << "\nLCM of "<< a << " and " << b << " is: " << lcm(a, b);
     return 0;
 }
+
+// Time Complexity = O(log(min(a, b)));
